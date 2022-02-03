290625fc1f0815b97ba7e5534c6e96b6a8a0c449
Gabriel Schincariol <gschin11@gmail.com>
2022-02-03 20:11:05 -0300

    Controllers do app

---
+++ Clientes.WebAPI/Clientes.WebAPI/Controllers/ClienteController.cs
@@ -0,0 +1,40 @@
+﻿using Clientes.WebAPI.Dal.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clientes.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ICliente _cliente;
+
+        public ClienteController(ICliente cliente)
+        {
+            this._cliente = cliente;
+        }
+        [HttpGet]
+        public Task<List<Model.Cliente>> Get()
+        {
+            return _cliente.GetClientes();
+        }
+        [HttpGet("{id}")]
+        public Task<Model.Cliente> Get(string id)
+        {
+            return _cliente.GetCliente(id);
+        }
+        [HttpPost]
+        public string Post([FromBody] Model.Cliente cliente) => _cliente.AddCliente(cliente);
+        [HttpPut]
+        public void Put([FromBody] Model.Cliente cliente)
+        {
+            _cliente.EditarCliente(cliente);
+        }
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            _cliente.ExcluirCliente(id);
+        }
+    }
+}
