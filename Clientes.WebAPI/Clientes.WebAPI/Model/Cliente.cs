e6e89ece2c376b279c0af4b7f528e9e912efefac
Gabriel Schincariol <gschin11@gmail.com>
2022-02-03 19:19:16 -0300

    first commit

---
+++ Clientes.WebAPI/Clientes.WebAPI/Model/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clientes.WebAPI.Model
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        [MaxLength(30)]
+        public string Nome { get; set; }
+        [MaxLength(100)]
+        public string Sobrenome { get; set; }
+        [MaxLength(11)]
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int Idade { get; set; }
+        public int Profissao { get; set; }
+    }
+}
