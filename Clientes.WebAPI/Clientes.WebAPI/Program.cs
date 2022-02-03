464612143798a5fcd5765af21795fa824225bdbb
Gabriel Schincariol <gschin11@gmail.com>
2022-02-03 20:12:54 -0300

    Configurações e adição do Swagger

--- Clientes.WebAPI/Clientes.WebAPI/Program.cs
+++ Clientes.WebAPI/Clientes.WebAPI/Program.cs
@@ -1,24 +1,47 @@
+using Clientes.WebAPI.Dal;
+using Clientes.WebAPI.Dal.Repositories;
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddCors();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddTransient<ICliente, Cliente>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials());
 
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{Id?}");
 
 app.MapRazorPages();
 
