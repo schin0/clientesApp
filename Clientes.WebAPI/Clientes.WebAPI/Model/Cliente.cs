f6d5ee469ba3268e7548de123de75644498639f7
Gabriel Schincariol <gschin11@gmail.com>
2022-02-03 20:11:14 -0300

    Model do app

--- Clientes.WebAPI/Clientes.WebAPI/Model/Cliente.cs
+++ Clientes.WebAPI/Clientes.WebAPI/Model/Cliente.cs
@@ -1,18 +1,36 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Google.Cloud.Firestore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Clientes.WebAPI.Model
 {
+    [FirestoreData]
     public class Cliente
     {
-        public int Id { get; set; }
+        [FirestoreProperty]
+        public string Id { get; set; }
+        
+        [FirestoreProperty]
+        public int IdCliente { get; set; }
+
         [MaxLength(30)]
+        [FirestoreProperty]
         public string Nome { get; set; }
+
         [MaxLength(100)]
+        [FirestoreProperty]
         public string Sobrenome { get; set; }
+
+        [FirestoreProperty]
         [MaxLength(11)]
         public string Cpf { get; set; }
+
+        [FirestoreProperty]
         public DateTime DataNascimento { get; set; }
+
+        [FirestoreProperty]
         public int Idade { get; set; }
+
+        [FirestoreProperty]
         public int Profissao { get; set; }
     }
 }
