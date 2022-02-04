cba3945b79330be0e2f72392cfc04ffbc5fe2aaf
Gabriel Schincariol <gschin11@gmail.com>
2022-02-04 12:01:00 -0300

    Importações

--- clientesApp/src/app/app.module.ts
+++ clientesApp/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { NavComponent } from './template/nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListarComponent } from './components/listar/listar.component';
+import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
+import { ExcluirComponent } from './components/excluir/excluir.component';
+import { EditarComponent } from './components/editar/editar.component';
+import { VisualizarComponent } from './components/visualizar/visualizar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    ListarComponent,
+    CadastrarComponent,
+    ExcluirComponent,
+    EditarComponent,
+    VisualizarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
