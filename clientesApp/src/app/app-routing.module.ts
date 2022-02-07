59eb5ce1d8490c5e867a54261afa865a586e74cb
Gabriel Schincariol <gschin11@gmail.com>
2022-02-07 17:33:03 -0300

    Novas rotas

--- clientesApp/src/app/app-routing.module.ts
+++ clientesApp/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
+import { InicioComponent } from './views/inicio/inicio.component';
+import { ExcluirComponent } from './components/excluir/excluir.component';
+import { VisualizarComponent } from './components/visualizar/visualizar.component';
+import { EditarComponent } from './components/editar/editar.component';
+import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
+import { ListarComponent } from './components/listar/listar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent
+  },
+  {
+    path: 'listar',
+    component: ListarComponent
+  },
+  {
+    path: 'cadastrar',
+    component: CadastrarComponent
+  },
+  {
+    path: 'editar/:id',
+    component: EditarComponent
+  },
+  {
+    path: 'visualizar/:id',
+    component: VisualizarComponent
+  },
+  {
+    path: 'excluir/:id',
+    component: ExcluirComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
