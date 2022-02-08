c18b0813deea3b5478c1fa930b8b769691bfbfd8
Gabriel Schincariol <gschin11@gmail.com>
2022-02-08 01:12:38 -0300

    Inclusão dos métodos restantes do CRUD

--- clientesApp/src/app/components/visualizar/visualizar.component.ts
+++ clientesApp/src/app/components/visualizar/visualizar.component.ts
@@ -1,3 +1,7 @@
+import { Router, ActivatedRoute } from '@angular/router';
+import { ClientesService } from './../../shared/clientes.service';
+import { MensagemService } from './../../shared/mensagem/mensagem.service';
+import { Cliente } from './../../shared/interfaces/ICliente';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualizar.component.scss']
 })
 export class VisualizarComponent implements OnInit {
-
-  constructor() { }
+  objValues: Cliente = {
+    "nome": '',
+    "sobrenome": '',
+    "cpf": '',
+    "dataNascimento": '',
+    "idade": 0,
+    "profissao": ''
+  }
+  constructor(private clienteService: ClientesService, private router: Router,
+    private route: ActivatedRoute, private mensagem: MensagemService) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.params["id"];
+    this.clienteService.buscarPeloId(id).subscribe(dados => {
+      this.objValues = dados;
+    });
   }
 
 }
